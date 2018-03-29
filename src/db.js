6fab3dd1a05051d3abfcc0b0e50e7120554c8292
rrsilaya <rrsilaya@up.edu.ph>
2018-03-29 17:56:24 +0800

    Make database credentials dynamic

--- src/db.js
+++ src/db.js
@@ -2,11 +2,13 @@ import mongoose from 'mongoose';
 
 mongoose.Promise = global.Promise;
 
-const db = mongoose.createConnection('mongodb://yses:yses@ds227459.mlab.com:27459/nominations');
+const { USER, PASSWORD } = process.env;
+
+const db = mongoose.createConnection(`mongodb://${USER}:${PASSWORD}@ds227459.mlab.com:27459/nominations`);
 
 db.on('error', err => console.log(err.message));
 db.once('open', () => {
-  console.log('Successfully connected to MongoDB');
+  console.log(`Successfully connected to MongoDB (username: ${USER})`);
 });
 
 export default db;
