8e8ef167769097cc503dd26c43ab6766c1ab63c9
rrsilaya <rrsilaya@up.edu.ph>
2018-03-29 16:48:39 +0800

    Connect mongodb to mlab

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 mongoose.Promise = global.Promise;
 
-const db = mongoose.createConnection('mongodb://localhost/nomsys');
+const db = mongoose.createConnection('mongodb://yses:yses@ds227459.mlab.com:27459/nominations');
 
 db.on('error', err => console.log(err.message));
 db.once('open', () => {
